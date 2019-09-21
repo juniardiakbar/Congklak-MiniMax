1ee234bbbab53d24c29264815c76b254d128e802
irfansofyana <irfansofyana0305@gmail.com>
2019-09-21 15:52:33 +0700

    add livestream vscode

--- congklakUtils.js
+++ congklakUtils.js
@@ -4,3 +4,4 @@ export const PLAYER2_HOME_NUMBER = 15;
 export const PLAYER1_HOLE_NUMBERS = [0, 1, 2, 3, 4, 5, 6];
 export const PLAYER2_HOLE_NUMBERS = [8, 9, 10, 11, 12, 13, 14];
 
+prod.liveshare.vsengsaas.visualstudio.com/join?B6510FCB7BCB25F05ED48F244724A861A16B
