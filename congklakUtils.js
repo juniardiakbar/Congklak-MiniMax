1404ee7f1ee3f8ff5157a09ec4a063708afad037
irfansofyana <irfansofyana0305@gmail.com>
2019-09-21 15:44:56 +0700

    add congklakUtils.js

---
+++ congklakUtils.js
@@ -0,0 +1,6 @@
+export const PLAYER1_HOME_NUMBER = 7;
+export const PLAYER2_HOME_NUMBER = 15;
+
+export const PLAYER1_HOLE_NUMBERS = [0, 1, 2, 3, 4, 5, 6];
+export const PLAYER2_HOLE_NUMBERS = [8, 9, 10, 11, 12, 13, 14];
+
