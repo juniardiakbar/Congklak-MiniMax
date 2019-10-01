23dcc3c070d4301553a52ada09565f310344dcf4
Juniardi Akbar <juniardiakbar@gmail.com>
2019-10-01 16:07:47 +0700

    fix ai vs player and bot vs player

--- public/script.js
+++ public/script.js
@@ -3,15 +3,27 @@ import { getCongklakNextState } from './coreLogic.js';
 import { getChoice } from './alphaBetaPrunning.js';
 import { simulateCongklakMove } from './simulateCongklakMove.js';
 
+var buttonPlay =  document.getElementById('play');
+var formType = document.getElementsByClassName('form-type');
+var formDifficulty = document.getElementsByClassName('form-difficulty');
+var type = 1;
+var difficulty = 2;
+
 var userHole = document.getElementsByClassName('user-hole');
 var userHomeHole = document.getElementsByClassName('user-home-hole');
 var aiHole = document.getElementsByClassName('ai-hole');
 var aiHomeHole = document.getElementsByClassName('ai-home-hole');
-var buttonPlay =  document.getElementById('play');
 var seedsLeft =  document.getElementById('seeds-left');
+var playingStatus = document.getElementById('playing-status');
+var buttonPlayGame = document.getElementById('play-game');
 var playing = 1;
 
-const holes = [];
+var beginningPage = document.getElementsByClassName('beginning-page');
+var mainPage = document.getElementsByClassName('main-page');
+
+var holes = [];
+var congklakState;
+
 for (let i=0; i<7; i++) {
   holes.push(userHole[i]);
 }
@@ -21,81 +33,91 @@ for (let i=0; i<7; i++) {
 }
 holes.push(aiHomeHole[0]);
 
-const congklakState = generateCongklakInitialState();
-congklakState.forEach((state, i) => {
-  holes[i].innerHTML = state;
-});
-
-
-for (let i=0; i<7; i++) {
-  userHole[i].addEventListener('click', async function() {
-    await simulateCongklakMove(congklakState, 1, i, holes, seedsLeft);
-    
-    const result = await getCongklakNextState(congklakState, 1, i);
-    const nextState = result.nextState;
-    playing = result.nextTurn;
-    
-    for (let i=0; i<16; i++) {
-      congklakState[i] = nextState[i];
+for (let i=0; i<3; i++) {
+  formType[i].addEventListener('change', function() {
+    type = parseInt(formType[i].value);
+    if (type != 3) {
+      buttonPlayGame.classList.add('hidden');
     }
-    
-    // if (playing == 2) {
-    //   await aiPlaying(playing, congklakState, holes, seedsLeft)
-    // }
-
   });
 }
 
-buttonPlay.addEventListener('click', async function(){
-    var holeNumber = await getChoice(congklakState, 4);
-    await simulateCongklakMove(congklakState, 2, holeNumber, holes, seedsLeft);
-    const result = await getCongklakNextState(congklakState, 2, holeNumber);
-    const nextState = result.nextState;
+for (let i=0; i<3; i++) {
+  formDifficulty[i].addEventListener('change', function() {
+    difficulty = parseInt(formDifficulty[i].value)*2;
+  });
+}
+
+buttonPlay.addEventListener('click', function() {  
+  congklakState = generateCongklakInitialState();
+  congklakState.forEach((state, i) => {
+    holes[i].innerHTML = state;
+  });
+  mainPage[0].classList.remove('hidden');
+  beginningPage[0].classList.add('hidden');
+});
+
+buttonPlayGame.addEventListener('click', async function() {
+  if (type == 3) {
+    var tempState = [...congklakState];
+    const randomHole = await getChoice(congklakState, 1);
+  
+    var result = await getCongklakNextState(congklakState, 1, randomHole);
+    var nextState = result.nextState;
     playing = result.nextTurn;
     
     for (let i=0; i<16; i++) {
       congklakState[i] = nextState[i];
     }
-});
-
-// async function botAiTurn(
-//   playing,
-//   congklakState,
-//   level,
-//   holeNumber,
-//   holes,
-//   seedsLeft,
-// ){
-//   if (playing == 2){
-//     var holeNumber = await getChoice(congklakState, level);
-//     await simulateCongklakMove(congklakState, 2, holeNumber, holes, seedsLeft);
-//     const result = await getCongklakNextState(congklakState, 2, holeNumber);
-//     const nextState = result.nextState;
-//     playing = result.nextTurn;
     
-//     for (let i=0; i<16; i++) {
-//       congklakState[i] = nextState[i];
-//     }
-//     holeNumber = await getChoice(congklakState, 4);
-//     return botAiTurn(playing, congklakState);
-//   }
-// }
+    var botHoles = [];
+  
+    while (playing == 2) {
+      const selectedHole = await getChoice(congklakState, difficulty);
+      botHoles.push(selectedHole);
+      var result = await getCongklakNextState(congklakState, 2, selectedHole);
+      var nextState = result.nextState;
+      playing = result.nextTurn;
+      
+      for (let i=0; i<16; i++) {
+        congklakState[i] = nextState[i];
+      }
+    }
+    console.log(botHoles);
+    await simulateCongklakMove(tempState, 1, i, holes, seedsLeft, playingStatus, botHoles);
+  }
+})
 
+for (let i=0; i<7; i++) {
+  userHole[i].addEventListener('click', async function() {
+    if (type == 1 || type == 2) {
+      var tempState = [...congklakState];
+      var result = await getCongklakNextState(congklakState, 1, i);
+      var nextState = result.nextState;
+      playing = result.nextTurn;
+      
+      for (let j=0; j<16; j++) {
+        congklakState[j] = nextState[j];
+      }
+      
+      var botHoles = [];
 
-
-// async function aiPlaying(playing, congklakState, holes, seedsLeft) {
-//   if (playing == 2) {
-//     var holeNumber = await getChoice(congklakState, 4);
-//     await simulateCongklakMove(congklakState, 2, holeNumber, holes, seedsLeft);
-//     const result = await getCongklakNextState(congklakState, 2, holeNumber);
-//     const nextState = result.nextState;
-//     playing = result.nextTurn;
-    
-//     for (let i=0; i<16; i++) {
-//       congklakState[i] = nextState[i];
-//     }
-//     aiPlaying(playing, congklakState, holes, seedsLeft);
-//   } else {
-//     return { congklakState, playing }
-//   }
-// }
+      while (playing == 2) {
+        if (type == 2) {
+          difficulty = 1;
+        }
+        const selectedHole = await getChoice(congklakState, difficulty);
+        botHoles.push(selectedHole);
+        var result = await getCongklakNextState(congklakState, 2, selectedHole);
+        var nextState = result.nextState;
+        playing = result.nextTurn;
+        
+        for (let i=0; i<16; i++) {
+          congklakState[i] = nextState[i];
+        }
+      }
+      console.log(botHoles);
+      await simulateCongklakMove(tempState, 1, i, holes, seedsLeft, playingStatus, botHoles);
+    }
+  });
+}
